3896e9e535453bf59950a6ae718fc40e5068409b
alls <alessiodevito73@gmail.com>
2025-03-18 13:13:56 +0100

    update

--- scripts/power.sh
+++ scripts/power.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-# -    bindsym l exec --no-startup-id dm-tool lock, mode "default"
-case "$(echo -e "Shutdown\nRestart\nLogout\nLock" | dmenu \
+case "$(echo -e "Poweroff\nreBoot\nloGout\nlocK" | dmenu \
     -nb "#191724" \
     -nf "#908caa" \
     -sb "#191724" \
     -sf "#9ccfd8" \
     -fn "JetBrains Mono Nerd Font-15" \
     -l 5 -i -p "Power Menu:")" in
-        Shutdown) exec systemctl poweroff;;
-        Restart) exec systemctl reboot;;
-        Logout) exec i3-msg exit;;
-        Lock) exec i3lock --image=/home/alls/Pictures/space.png;;
+        Poweroff) exec systemctl poweroff;;
+        reBoot) exec systemctl reboot;;
+        loGout) exec i3-msg exit;;
+        locK) exec i3lock --image=/home/alls/Pictures/space.png;;
 esac
