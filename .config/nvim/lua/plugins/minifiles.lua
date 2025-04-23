2f6084b69392cc4eab915e507ae4ab8909be9c5b
alls <alessiodevito73@gmail.com>
2025-04-23 11:07:39 +0200

    update

--- .config/nvim/lua/plugins/minifiles.lua
+++ .config/nvim/lua/plugins/minifiles.lua
@@ -1,6 +1,6 @@
 return {
-	'echasnovski/mini.files',
-	version = '*',
+	"echasnovski/mini.files",
+	version = "*",
 	config = function()
 		require("mini.files").setup({
 			options = {
@@ -8,27 +8,32 @@ return {
 				use_as_default_explorer = true,
 			},
 			mappings = {
-				close       = 'q',
-				go_in       = 'l',
-				go_in_plus  = '',
-				go_out      = 'h',
-				go_out_plus = '',
-				mark_goto   = "",
-				mark_set    = '',
-				reset       = '',
-				reveal_cwd  = '',
-				show_help   = 'g?',
-				synchronize = '=',
-				trim_left   = '<',
-				trim_right  = '>',
+				close = "q",
+				go_in = "l",
+				go_in_plus = "",
+				go_out = "h",
+				go_out_plus = "",
+				mark_goto = "",
+				mark_set = "",
+				reset = "",
+				reveal_cwd = "",
+				show_help = "g?",
+				synchronize = "=",
+				trim_left = "<",
+				trim_right = ">",
 			},
 			windows = {
 				preview = true,
 				width_focus = 25,
-				width_preview = 55
+				width_preview = 55,
 			},
 		})
 
-		vim.keymap.set("n", "<leader>b", ":lua MiniFiles.open()<cr>", { desc = "MiniFiles Open", noremap = true, silent = false })
+		vim.keymap.set(
+			"n",
+			"<leader>b",
+			":lua MiniFiles.open()<cr>",
+			{ desc = "MiniFiles Open", noremap = true, silent = false }
+		)
 	end,
 }
--- .vimrc
+++ .vimrc
@@ -1,4 +1,4 @@
-set ts=4 sw=4 ai cul nu rnu mps+=<:> hls noswf so=8 
+set ts=4 sw=4 ai cul nu rnu mps+=<:> hls noswf so=8 bg=dark
 syntax on
 let mapleader=" "
 vnoremap <leader>y "+y
@@ -6,6 +6,5 @@ nnoremap <leader>s :s/\<<C-r><C-w>\>//gc<Left><Left><Left>
 noremap x "_x
 vnoremap x "_x
 autocmd TextChanged,TextChangedI * silent! write
-colorscheme retrobox
-nnoremap <leader>b :ls<CR>:b<space>
+colorscheme habamax
 nnoremap <esc> :noh<CR>
