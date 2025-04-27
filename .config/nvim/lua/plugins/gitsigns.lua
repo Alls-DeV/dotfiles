84e30ec3bdb29d81271c8948a766feb51f471d9f
alls <alessiodevito73@gmail.com>
2025-04-27 11:14:38 +0200

    update

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -26,7 +26,7 @@ return {
 				end
 
 				-- Navigation
-				map("n", "]g", function()
+				map("n", "]h", function()
 					if vim.wo.diff then
 						vim.cmd.normal({ "]c", bang = true })
 					else
@@ -34,7 +34,7 @@ return {
 					end
 				end)
 
-				map("n", "[g", function()
+				map("n", "[h", function()
 					if vim.wo.diff then
 						vim.cmd.normal({ "[c", bang = true })
 					else
@@ -42,16 +42,26 @@ return {
 					end
 				end)
 
-				-- Actions
-				map("n", "<leader>gr", gitsigns.reset_hunk)
-				map("n", "<leader>gp", gitsigns.preview_hunk)
-				map("n", "<leader>gb", function()
+				map("n", "<leader>hs", gitsigns.stage_hunk)
+				map("n", "<leader>hr", gitsigns.reset_hunk)
+
+				map("v", "<leader>hs", function()
+					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+
+				map("v", "<leader>hr", function()
+					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+
+				map("n", "<leader>hS", gitsigns.stage_buffer)
+				map("n", "<leader>hR", gitsigns.reset_buffer)
+				map("n", "<leader>hi", gitsigns.preview_hunk_inline)
+
+				map("n", "<leader>hb", function()
 					gitsigns.blame_line({ full = true })
 				end)
-				map("n", "<leader>gd", gitsigns.diffthis)
-				map("n", "<leader>gD", function()
-					gitsigns.diffthis("~")
-				end)
+
+				map("n", "<leader>hd", gitsigns.diffthis)
 			end,
 		})
 	end,
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,35 @@
-return { 
+return {
 	{
-		'nvim-telescope/telescope.nvim', branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-symbols.nvim', { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } },
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-symbols.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
 		config = function()
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-			vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-            vim.keymap.set('n', '<leader>sd', ':Telescope diagnostics bufnr=0<CR>', { desc = '[S]earch [d]iagnostics in actual buffer' })
-            vim.keymap.set('n', '<leader>sD', builtin.diagnostics, { desc = '[S]earch [D]iagnostics in the entire workspace' })
-            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+			require("telescope").setup({
+				defaults = {
+					layout_strategy = "vertical",
+				},
+			})
+
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set(
+				"n",
+				"<leader>sd",
+				":Telescope diagnostics bufnr=0<CR>",
+				{ desc = "[S]earch [d]iagnostics in actual buffer" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>sD",
+				builtin.diagnostics,
+				{ desc = "[S]earch [D]iagnostics in the entire workspace" }
+			)
+			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		end,
-	}
+	},
 }
